f773220fbc6340b5299ce4a8bf76af9ce3575df6
espelho-07 <darpanparmar1707@gmail.com>
2025-02-10 22:14:38 +0530

    Java

--- Try.java
+++ Try.java
@@ -1,9 +1,40 @@
 import java.util.Scanner;
 class Train{
     int Train_No.;
-    int Train_Name;
-    
+    String Train_Name;
+    int No_Of_Coach;
+    String[] Stations;
+    int[] distance;
 }
+public Train(int Train_No,String Train_Name,int No_Of_Coach,String Train_Type,int PNR_No){
+    this. Train_No=Train_No;
+    this. Train_Name=Train_Name;
+    this.No_Of_Coach=No_Of_Coach;
+    this. String[] Stations=String[] Stations;
+    this. int[] distance=int[] distance;
+}
+
+class Coach{
+    String Coach_Type;
+}
+public Coach(String Coach_Type){
+    this. Coach_Type= Coach_Type;
+}
+
+class Passanger{
+    String Passanger_Name;
+    String E-Mail;
+    int Mobile_No;
+    String Gender;
+    int Age;
+}
+public Passanger(String Passanger_Name,String E-Mail,int Mobile_No,){
+    this.E-Mail= E-Mail;
+    this.Passanger_Name= Passanger_Name;
+    this.Mobile_No= Mobile_No;
+
+}
+
 public class Try{
     public static void main(String[] args){
 
